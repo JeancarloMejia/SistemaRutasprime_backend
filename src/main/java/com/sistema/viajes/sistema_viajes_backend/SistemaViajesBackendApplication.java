33b156e1919d7cbe0376428a6a04efadd8f57686
Jeancarlo Mejia <u23211982@utp.edu.pe>
2025-09-02 04:54:09 -0500

    Agregando

---
+++ src/main/java/com/sistema/viajes/sistema_viajes_backend/SistemaViajesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.viajes.sistema_viajes_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaViajesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaViajesBackendApplication.class, args);
+	}
+
+}
